0802625795b82937a9ca9c14b6a6a21205f90fe7
SIonut0122 <sionut0122@yahoo.com>
2020-08-26 21:07:23 +0200

    Updates

---
+++ precache-manifest.d899fcf9b4864c9a18d2f69789b2fca6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9816899bcfb1e8a063dd5f61c3100f4",
+    "url": "/paymentDTO/index.html"
+  },
+  {
+    "revision": "9d84ec0de3fbce4408d1",
+    "url": "/paymentDTO/static/css/main.80216547.chunk.css"
+  },
+  {
+    "revision": "541fd4cdd97c3cd47613",
+    "url": "/paymentDTO/static/js/2.d8650175.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/paymentDTO/static/js/2.d8650175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d84ec0de3fbce4408d1",
+    "url": "/paymentDTO/static/js/main.4be400d2.chunk.js"
+  },
+  {
+    "revision": "e7c863eb644b6913ff91",
+    "url": "/paymentDTO/static/js/runtime-main.9e04559a.js"
+  },
+  {
+    "revision": "18b7d4ead1a06c3a32e2d8957ec398e6",
+    "url": "/paymentDTO/static/media/accepted_cards.18b7d4ea.png"
+  },
+  {
+    "revision": "dc9d2f698bd5ce24d1731f57addc3a2e",
+    "url": "/paymentDTO/static/media/check_mark.dc9d2f69.png"
+  }
+]);
